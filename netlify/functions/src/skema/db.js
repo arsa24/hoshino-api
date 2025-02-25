795e6516d91cd59af1fa4bd5bda0e64a298ac696
arsa24 <idunvoneinzbern@gmail.com>
2025-02-25 19:15:03 +0700

    fix: fix error file not found

--- netlify/functions/src/skema/db.js
+++ netlify/functions/src/skema/db.js
@@ -2,6 +2,11 @@ const fs = require("fs");
 const path = require("path");
 const dataPath = path.join(__dirname + "/data/users.json");
 
+if (!fs.existsSync(dataPath)) {
+  fs.mkdirSync(path.dirname(dataPath), { recursive: true });
+  fs.writeFileSync(dataPath, JSON.stringify([]), "utf8");
+}
+
 function readUsers() {
   const data = fs.readdirSync(dataPath, "utf8");
   return data;
