620de7ca7bd06e879de7834c603ff0ddab7e8732
arsa24 <idunvoneinzbern@gmail.com>
2025-02-26 08:39:19 +0700

    update dikit

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,11 +2,16 @@ const express = require("express");
 const serverless = require("serverless-http");
 const { authenticateToken } = require("./src/skema/middleware");
 const authRoutes = require("./src/skema/auth");
+const { readUsers } = require("./src/skema/db");
 
 const api = express();
 api.use(express.json());
 
 api.use("/api/skema/auth", authRoutes);
+api.get("/api/skema/users", (req, res) => {
+  const users = readUsers()
+  res.json({ users })
+});
 
 api.get("/api/protected", authenticateToken, (req, res) => {
   res.json({ message: `Halo ${req.user.username}, ini adalah data rahasia!` });
--- netlify/functions/src/skema/db.js
+++ netlify/functions/src/skema/db.js
@@ -1,12 +1,27 @@
 const fs = require("fs");
+const path = require("path");
 
-function readUsers(dataPath) {
-  const data = fs.readdirSync(dataPath, "utf8");
-  return data;
+
+const dataPath = path.join(__dirname + "/users.json");
+
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, JSON.stringify([]), "utf8")
 }
 
-function writeUsers(dataPath, user) {
-  fs.writeFileSync(dataPath, JSON.stringify(user, null, 2), "utf8");
+function readUsers() {
+  try {
+    const data = fs.readFileSync(dataPath, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+function writeUsers(user) {
+  try {
+    fs.writeFileSync(dataPath, JSON.stringify(user, null, 2), "utf8");
+  } catch (e) { console.error(e) }
 }
 
 module.exports = { readUsers, writeUsers };
